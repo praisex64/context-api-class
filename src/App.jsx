a2bb3a6dfcb65dff0b55c389f23fd56182018bee
praisex64 <akpevweess1@gmail.com>
2023-05-15 12:10:38 +0100

    feat: added authetication to the app

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,23 @@ import ThemeContextProvider from './contexts/ThemeContext'
 import Navbar from './components/Navbar'
 import BookList from './components/BookList'
 import ThemeToggle from './components/ThemeToggle'; 
+import AuthContextProvider from './contexts/AuthContext';
 
 function App() {
  
 
   return (
    <div className="App">
+   <AuthContextProvider>
     <ThemeContextProvider>
-      <Navbar />
+          <Navbar />
+      
+  
       <BookList />
       <ThemeToggle />
     </ThemeContextProvider>
-     
+ 
+    </AuthContextProvider>
 
    </div>
       
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,32 +1,43 @@
-import React, { Component } from "react"
-import { ThemeContext } from "../contexts/ThemeContext"
+import  { Component } from 'react'; 
+import {ThemeContext } from '../contexts/ThemeContext'; 
+import { AuthContext } from '../contexts/AuthContext';
+
 
 class Navbar extends Component {
-     
+
     static contextType = ThemeContext; 
-
+     
     render() {
-        return(
-            <ThemeContext.Consumer>{(context) => {
-                const {isLightTheme, light, dark } = context; 
+        
+     return (
+        <AuthContext.Consumer>{(AuthContext) => (
+            <ThemeContext.Consumer>{(themecontext) => {
+                const { isAuthenticated, toggleAuth } = AuthContext; 
+                const { isLightTheme, light, dark } = themecontext; 
                 const theme = isLightTheme ? light : dark; 
-                  return (
-                    <nav style={{ background: theme.bg, color: theme.syntax, }}> 
-                        <h1>Context App</h1>
-                        <ul>
-                            <li>Home</li>
-                            <li>About</li>
-                            <li>Contact</li> 
-                        </ul>
-                    </nav>
-                  )
+               return (
+                            <nav style={{ background: theme.ui, color: theme.syntax}}>
 
-            }}
+                            <h1>Context App</h1>
+                            <div onClick={toggleAuth}>
+                                { isAuthenticated ? 'Logged in' : 'Logged out'}
+                            </div>
+                            <ul>
+                                <li>Home</li>
+                                <li>About</li>
+                                <li>Contact</li>
+                            </ul>
+                             </nav>
+               )}
+    
+                }</ThemeContext.Consumer>
 
-            </ThemeContext.Consumer>
-        ); 
+        )}
+           
+            </AuthContext.Consumer>
+        );
     }
-
 }
 
 export default Navbar; 
+
