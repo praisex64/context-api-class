8536aa3b7ea072162488a6a073bae850d0bbfbc9
praisex64 <akpevweess1@gmail.com>
2023-05-15 11:01:54 +0100

    feat: implemented the toggle button

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
-
-import './App.css'
+import ThemeContextProvider from './contexts/ThemeContext'
+import Navbar from './components/Navbar'
+import BookList from './components/BookList'
+import ThemeToggle from './components/ThemeToggle'; 
 
 function App() {
  
 
   return (
    <div className="App">
+    <ThemeContextProvider>
+      <Navbar />
+      <BookList />
+      <ThemeToggle />
+    </ThemeContextProvider>
      
 
    </div>
